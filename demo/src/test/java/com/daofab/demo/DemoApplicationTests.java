aa2d69872ff340ba87345a00e174b9c1b21574c0
samratshekhar42 <samratshekhar42@gmail.com>
2023-06-16 11:37:24 +0530

    initial commit with spring project

---
+++ demo/src/test/java/com/daofab/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daofab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
