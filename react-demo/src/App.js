6f46cfe260a8570c3bdb915ac1db178b1fd87703
samratshekhar42 <samratshekhar42@gmail.com>
2023-06-16 14:01:33 +0530

    add react project

---
+++ react-demo/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ParentList from './components/ParentList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ParentList />
+    </div>
+  );
+}
+
+export default App;
