b388fc3bddd0fa13c9a73cfaeae9aa3929ca325d
samratshekhar42 <samratshekhar42@gmail.com>
2023-06-16 12:51:26 +0530

    add child model

---
+++ demo/src/main/java/com/daofab/demo/model/Child.java
@@ -0,0 +1,12 @@
+package com.daofab.demo.model;
+
+import lombok.Data;
+
+@Data
+public class Child {
+
+    private int id;
+    private int parentId;
+    private int paidAmount;
+
+}
---
+++ demo/src/main/java/com/daofab/demo/model/ChildData.java
@@ -0,0 +1,11 @@
+package com.daofab.demo.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class ChildData {
+
+    List<Child> data;
+}
